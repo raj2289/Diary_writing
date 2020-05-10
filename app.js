236608184abffea6b762ec04404c9957be5060e5
unknown <rajpatankar89@gmail.com>
2020-05-10 16:46:14 +0530

    Addgitignore ,and update

--- app.js
+++ app.js
@@ -159,7 +159,15 @@ app.post("/singup",function(req,res){
   }
 });
 });
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port=process.env.PORT;
+if(port==null || port =="")
+{
+  port=3000;
+}
+app.listen(port,function(){
+  console.log(port);
+  console.log("server is started");
 });
+//app.listen(3000, function() {
+  //console.log("Server started on port 3000");
+//});
